dbf2472b993f70763c9fddc1c79f0737e482882c
karunakar-smsc <karunakar.vemana@smscountry.com>
2020-01-22 16:40:14 +0530

    angular_

---
+++ Angular_CRUD/Controllers/HomeController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Angular_CRUD.Models;
+using System.Configuration;
+
+
+namespace Angular_CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public JsonResult GetName(string Name)
+        {
+
+                var cn = new SqlConnection();
+                var ds = new DataSet();
+                string strCn = ConfigurationManager.ConnectionStrings["testconnection"].ToString();
+                cn.ConnectionString = strCn;
+                var cmd = new SqlCommand
+                {
+                    Connection = cn,
+                    CommandType = CommandType.Text,
+                    CommandText = "select Emp_Name from Employee Where Emp_Name like @myParameter and Emp_Name!=@myParameter2"
+                };
+                cmd.Parameters.AddWithValue("@myParameter", "%" + Name + "%");
+                cmd.Parameters.AddWithValue("@myParameter2",  Name );
+                try
+                {
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    var da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    cn.Close();
+                }
+                DataTable dt = ds.Tables[0];
+
+
+                var txtItems = (from DataRow row in dt.Rows
+                                select row["Emp_Name"].ToString()
+                                    into dbValues
+                                    select dbValues.ToLower()).ToList();
+                return Json(txtItems, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //get all data
+        public JsonResult GetAll()
+        {
+            using (testEntities1 db = new testEntities1())
+            {
+                List<Employee> emp = db.Employees.ToList();
+                return Json(emp, JsonRequestBehavior.AllowGet);
+            }
+            
+        }
+        // get by id
+        public JsonResult GetById(string id)
+        {
+            using (testEntities1 db = new testEntities1())
+            {
+                int emp_id = int.Parse(id);
+                return Json(db.Employees.Find(emp_id), JsonRequestBehavior.AllowGet);
+            }
+        }
+        public string Insert_data(Employee empl)
+        {
+            if(empl != null)
+            {
+                using(testEntities1 db =new testEntities1())
+                {
+                    db.Employees.Add(empl);
+                    db.SaveChanges();
+                    return "Added Successfully";
+                }
+            }
+            else
+            {
+                return "try again";
+            }
+        }
+        
+        public string Delete_emp(Employee Emp )
+        {
+            if (Emp != null)
+            {
+                using (testEntities1 Obj = new testEntities1())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Employees.Attach(Emp);
+                        Obj.Employees.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }  
+            //if (id != null)
+            //{
+            //    var id_ = int.Parse(id);
+            //    using(testEntities1 db= new testEntities1())
+            //    {
+            //        var Emp_ = db.Employees.Where(a => a.Emp_Id == id_).ToList();
+            //        foreach (var item in Emp_)
+            //        {
+            //            db.Employees.Remove(item);
+            //        }
+            //        db.SaveChanges();
+            //    }
+            //    return "Employee Deleted id:" +id_;
+            //}
+            //else
+            //{
+            //    return "Try Again id:" + id;
+            //}
+        }
+        public string update(Employee emp)
+        {
+            if (emp != null)
+            {
+                using(testEntities1 db = new testEntities1())
+                {
+                    var Empl = db.Entry(emp);
+                    Employee empobj = db.Employees.Where(x => x.Emp_Id == emp.Emp_Id).FirstOrDefault();
+                    empobj.Emp_Name = emp.Emp_Name;
+                    empobj.Emp_City = emp.Emp_City;
+                    empobj.Emp_Age = emp.Emp_Age;
+                    db.SaveChanges();
+                }
+                return "updated";
+            }
+            else
+            {
+                return "try again";
+            }
+        }
+    }
+}
---
+++ Angular_CRUD/Scripts/CustomeJS/Employee.js
@@ -0,0 +1,95 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+        $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            $scope.Employe = {};
+
+            if ($scope.EmpName == '' || $scope.EmpName == undefined) {
+                alert("Please enter name ");
+                return false;
+            }
+            else {
+                $scope.Employe.Emp_Name = $scope.EmpName;
+            }
+            if ($scope.EmpCity == '' || $scope.EmpCity == undefined) {
+                alert("Please enter city ");
+                return false;
+            }
+            else {
+                $scope.Employe.Emp_City = $scope.EmpCity;
+            }
+            if ($scope.EmpAge == '' || $scope.EmpAge == undefined) {
+                alert("Please enter age ");
+                return false;
+            }
+            else {
+                $scope.Employe.Emp_Age = $scope.EmpAge;
+            }             
+               $http({
+                method: "post",
+                url: "/Home/Insert_data",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+            })
+        } else {
+            $scope.Employe = {};
+            $scope.Employe.Emp_Name = $scope.EmpName;
+            $scope.Employe.Emp_City = $scope.EmpCity;
+            $scope.Employe.Emp_Age = $scope.EmpAge;
+            $scope.Employe.Emp_Id = document.getElementById("EmpID_").value;
+            $http({
+                method: "post",
+                url: "/Home/update",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Employee";
+            })
+        }
+    }
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "/Home/GetAll"
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    $scope.DeleteEmp = function (Emp) {
+        debugger;
+        $http({
+            method: "post",
+            url: "/Home/Delete_emp",
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAllData();
+        })
+    };
+    $scope.UpdateEmp = function (Emp) {
+        document.getElementById("EmpID_").value = Emp.Emp_Id;
+        $scope.EmpName = Emp.Emp_Name;
+        $scope.EmpCity = Emp.Emp_City;
+        $scope.EmpAge = Emp.Emp_Age;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Green";
+        document.getElementById("spn").innerHTML = "Update Employee Information";
+    }
+})
